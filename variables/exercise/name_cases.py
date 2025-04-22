75c0a6e784eccaf8aeac060597a7823bffae8d51
SuptoHossein <supto.hossein@gmail.com>
2025-04-22 16:40:40 +0600

    exercise files

---
+++ variables/exercise/name_cases.py
@@ -0,0 +1,8 @@
+# variable
+# name = "Hello Eric, would you like to learn some Python today?"
+# print(name)
+
+# personal_name = "shourav"
+# print(personal_name.title(), personal_name.upper(), personal_name.lower())
+
+quote = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
